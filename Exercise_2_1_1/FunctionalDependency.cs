176d6acbde3ac346246ec1d3ac453053d4889782
Henrik Bulskov <bulskov@ruc.dk>
2021-10-19 12:49:18 +0200

    Add project files.

---
+++ Exercise_2_1_1/FunctionalDependency.cs
@@ -0,0 +1,19 @@
+﻿namespace Exercise2_1
+{
+    public class FunctionalDependency
+    {
+        public FunctionalDependency(string lhs, string rhs)
+        {
+            LeftHandSide = new AttributeList(lhs);
+            RightHandSide = new AttributeList(rhs);
+        }
+
+        public AttributeList LeftHandSide { get; }
+        public AttributeList RightHandSide { get; }
+
+        public override string ToString()
+        {
+            return $"{{{LeftHandSide}}} -> {{{RightHandSide}}}";
+        }
+    }
+}
